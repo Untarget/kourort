fba5cec3c20c47fde71515f2463cf7ca0b6ac9ff
Untarget <workusotsuke@gmail.com>
2024-05-25 01:01:29 +0300

    промежуток

--- kourort/kourort/GeneralKourortUser.Designer.cs
+++ kourort/kourort/GeneralKourortUser.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lastnameLabel = new System.Windows.Forms.Label();
+            this.secondnameLabel = new System.Windows.Forms.Label();
+            this.firstnameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lastnameLabel
+            // 
+            this.lastnameLabel.AutoSize = true;
+            this.lastnameLabel.Location = new System.Drawing.Point(12, 65);
+            this.lastnameLabel.Name = "lastnameLabel";
+            this.lastnameLabel.Size = new System.Drawing.Size(54, 13);
+            this.lastnameLabel.TabIndex = 5;
+            this.lastnameLabel.Text = "Отчетсво";
+            // 
+            // secondnameLabel
+            // 
+            this.secondnameLabel.AutoSize = true;
+            this.secondnameLabel.Location = new System.Drawing.Point(12, 14);
+            this.secondnameLabel.Name = "secondnameLabel";
+            this.secondnameLabel.Size = new System.Drawing.Size(56, 13);
+            this.secondnameLabel.TabIndex = 4;
+            this.secondnameLabel.Text = "Фамилия";
+            // 
+            // firstnameLabel
+            // 
+            this.firstnameLabel.AutoSize = true;
+            this.firstnameLabel.Location = new System.Drawing.Point(12, 36);
+            this.firstnameLabel.Name = "firstnameLabel";
+            this.firstnameLabel.Size = new System.Drawing.Size(29, 13);
+            this.firstnameLabel.TabIndex = 3;
+            this.firstnameLabel.Text = "Имя";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Название оздоровительного лагеря:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // GeneralKourortUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lastnameLabel);
+            this.Controls.Add(this.secondnameLabel);
+            this.Controls.Add(this.firstnameLabel);
+            this.Name = "GeneralKourortUser";
             this.Text = "GeneralKourortUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lastnameLabel;
+        private System.Windows.Forms.Label secondnameLabel;
+        private System.Windows.Forms.Label firstnameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
